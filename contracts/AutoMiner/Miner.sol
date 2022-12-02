dfad89a07fcaf42aebdb5052e586a2ae385432ca
shlyn <shp32131@gmail.com>
2022-12-02 10:42:22 +0800

    feat: update Miner

--- contracts/AutoMiner/Miner.sol
+++ contracts/AutoMiner/Miner.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract AutoMiner {
-    receive() external payable {}
+import "./MinerStorage.sol";
+
+// import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
+// contract Miner is MinerStorage, Ownable, Initializable {
+contract Miner is MinerStorage {
+    receive() external payable {
+        memberFee += msg.value;
+    }
+
+    // function initialize(address _factory) external initializer {
+    // __Ownable_init();
+
+    // factory = _factory;
+    // minGasPrice = 100_000_000;
+    // taskCountPerMember = 5;
+    // }
 }
